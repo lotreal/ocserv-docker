8e5dd940ba58599d93a9e2e99fa91999060362ec
Luo Tao <lotreal@gmail.com>
2015-01-12 09:33:09 +0800

    boot script:run.sh:use relative etc dir

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-docker run -d --name vpn --privileged -v ~/ocserv-docker/ocserv:/etc/ocserv -p 10443:443/udp -p 10443:443/tcp wppurking/ocserv
+CMD="docker run -d --name vpn --privileged -v $BSDIR/ocserv:/etc/ocserv -p 10443:443/udp -p 10443:443/tcp wppurking/ocserv"
+
+echo $CMD && $CMD
